200a711a357c34147a8213290a56aa97ffed6b5c
Kadir Kağan Yüksel <101135338+Kadiryuksell@users.noreply.github.com>
2024-09-22 00:44:38 +0300

    Employee Created

--- MVCLibrary/Models/Entity/Employee.cs
+++ MVCLibrary/Models/Entity/Employee.cs
@@ -11,7 +11,8 @@ namespace MVCLibrary.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Employee
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,7 +22,10 @@ namespace MVCLibrary.Models.Entity
         }
     
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employee name can not be empty")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Employee last name can not be empty")]
         public string LastName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
