58c00de862f04b23c9d27b3770bb361834f49ef0
Kadir Kağan Yüksel <101135338+Kadiryuksell@users.noreply.github.com>
2024-09-27 18:29:59 +0300

    The dashboard template has been created

---
+++ MVCLibrary/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLibrary.Models.Entity;
+
+namespace MVCLibrary.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        
+        public ActionResult DashboardPage()
+        {
+            return View();
+        }
+    }
+}
