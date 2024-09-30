ad125fae77bd7940644cb95070a035fe8f73e07c
Kadir Kağan Yüksel <101135338+Kadiryuksell@users.noreply.github.com>
2024-09-30 20:06:24 +0300

    User Login & Register page template created

---
+++ MVCLibrary/Controllers/UserLoginOperationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLibrary.Models.Entity;
+
+namespace MVCLibrary.Controllers
+{
+    public class UserLoginOperationController : Controller
+    {
+        // GET: UserLogin
+        LibraryEntities db = new LibraryEntities();
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Users user) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register");
+            }
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+    }
+}
